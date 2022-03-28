36c58ac0c638c05f98c1fdb72b9f8ae70c37bf46
Hemant Kumar Sahu <hemantjava90@gmail.com>
2022-03-28 12:47:50 +0530

    Delete src directory

--- src/main/java/example/hemant/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package example.hemant.repository;
-
-import example.hemant.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
--- src/main/java/example/hemant/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package example.hemant.repository;
-
-import example.hemant.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
